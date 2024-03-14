f714d642a7a5664f9469fec0a90bdcace9903767
Norman Jäckel <contact@normanjaeckel.de>
2024-03-15 00:29:34 +0100

    Updated devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -11,8 +11,8 @@
   ];
 
   enterShell = ''
-    # ROCDIR="$(pwd)/$(find . -type d -iname 'roc_nightly*' | head -n 1)"
-    ROCDIR="$(pwd)/roc/target/release"
+    ROCDIR="$(pwd)/$(find . -type d -iname 'roc_nightly*' | head -n 1)"
+    # ROCDIR="$(pwd)/roc/target/release"
     PATH=$ROCDIR:$PATH
   '';
 }
